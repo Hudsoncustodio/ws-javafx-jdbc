58082d4b2b510b004f6aec717ebfadf79e9ae3b7
Hudson Ferreira <141644371+Hudsoncustodio@users.noreply.github.com>
2024-03-17 12:21:32 -0300

    Remove department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,4 +1,4 @@
-package model.services;
+/*package model.services;
 
 import java.util.List;
 
@@ -21,6 +21,39 @@ public class DepartmentService {
 		else {
 			dao.update(obj);
 		}
+	}
+	
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
+	}
+}*/
+package model.services;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentService {
+
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
+	public List<Department> findAll() {
+		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+	
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
 	}
 }
 
